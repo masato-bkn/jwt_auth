acd315cb37bf329057f15b3bb80d20e35d8358f4
重田 将人 <masato.0928.j@gmail.com>
2021-01-10 15:18:09 +0900

    POST /usersを作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,19 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  def create
+    user = User.create!(user_params)
+    render json: user
+  end
+
   def show
     user = User.find(params[:id])
     render json: user
+  end
+
+  private
+
+  def user_params
+    params.permit(:name, :email, :password)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, only: [:show]
+  resources :users, only: [:show, :create]
+  resources :tokens, only: [:create]
 end
