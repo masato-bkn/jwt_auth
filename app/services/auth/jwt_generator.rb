9a37b9da970b9d4174006847b88bef3e15ea8cec
重田 将人 <masato.0928.j@gmail.com>
2021-01-10 11:26:22 +0900

    トークン作成サービスであるAuth::JwtGeneratorを作成

---
+++ app/services/auth/jwt_generator.rb
@@ -0,0 +1,22 @@
+require 'jwt'
+
+class Auth::JwtGenerator
+    def initialize(user_id:, password:, email:)
+        @payload = payload(user_id, password, email)
+    end
+
+    def run
+        # TODO 秘密鍵、アルゴリズムは後で
+        JWT.encode(@payload, nil, 'none')
+    end
+
+    private
+
+    def payload(user_id, password, email)
+        {
+            user_id: user_id,
+            password: password,
+            email: email
+        }
+    end
+end
