40d3b9bad5903b2e0c71a5fe227b6c2fe4636580
重田 将人 <masato.0928.j@gmail.com>
2021-01-10 15:09:19 +0900

    GET /users/{:id}のテストを作成

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -20,4 +20,24 @@ RSpec.describe UsersController, type: :request do
       expect { subject }.to change(User, :count).by(1)
     end
   end
+
+  describe 'GET' do
+    subject do
+      get user_path(user.id)
+      response
+    end
+
+    let :user do
+      create(:user)
+    end
+
+    it '想定するレスポンスが返ってくること' do
+      result = JSON.parse(subject.body)
+
+      expect(result['id']).to eq(user.id)
+      expect(result['name']).to eq(user.name)
+      expect(result['email']).to eq(user.email)
+      expect(result['password_digest']).to eq(user.password_digest)
+    end
+  end
 end
